001f99ba2c0fc05618552c095c0641fba5eef5f3
Muhammad Nofil Ansari <nofil.ansari2k3@gmail.com>
2024-11-27 22:56:41 +0500

    Add files via upload

---
+++ My_bank.py
@@ -0,0 +1,67 @@
+bank_data = [{"CustomerName": "Nofil", "AccountNumber":245645,"AccountType":"Savings Account","BankBalance":10000}, {"CustomerName": "Mujtaba", "AccountNumber":24555,"AccountType":"Current Account","BankBalance":1000}]
+#Account_Number_value = int(input("Enter Account number: "))
+class Bank:
+    def __init__(self,Database,Account_Number_value):
+        self.database = Database
+        self.Account_Number_value = Account_Number_value        
+    def Search_All_Account_Getter(Database): 
+        for element in Database:
+            return element.values()    
+            #print(f"your account details are {element["CustomerName"], element["AccountNumber"],element["AccountType"]}")
+    #SearchGetter(Database)
+    def CreateAccount(Database):
+        global bank_data
+        CustomerName = input("Enter Customer Name: ")
+        AccountNumber = int(input("Enter Account Number: "))
+        AccountType = input("Enter Account type Current Account or Saving Account?: ")
+        Deposit_Amount = int(input("Enter Bank Deposit amount: "))
+        Updated_dict = {"CustomerName":CustomerName,"AccountNumber":AccountNumber,"AccountType":AccountType,"Deposit_Amount":Deposit_Amount}
+        bank_data.append(Updated_dict)
+        return f'Your Account has been added with details {Updated_dict}'            
+class Customer(Bank):
+    def __init__(self, Database):
+        super().__init__(Database) 
+    def BankBalanceGetter(Database,Account_Number_value):
+        for element in Database:
+            if element['AccountNumber'] == Account_Number_value:                
+                return f'Your Bank  Balance is {element["BankBalance"]}'
+        return None 
+    #print(BankBalanceGetter(Database))
+    def WithdrawSetter(Database,Account_Number_value):
+        withdraw_value = int(input("Enter withdraw amount: "))
+        for element in Database:
+            if element['AccountNumber'] == Account_Number_value:
+                updated_Database_value = element.get("BankBalance") - withdraw_value
+                element.update({"BankBalance":updated_Database_value})
+                return f'Your Bank Balance after withdraw is {element["BankBalance"]}'
+        return None 
+    #print(WithdrawSetter(Database))
+    def DepositSetter(Database,Account_Number_value):
+        Deposit_value = int(input("Enter withdraw amount: "))
+        for element in Database:
+            if element['AccountNumber'] == Account_Number_value:
+                updated_Database_value = element.get("BankBalance") + Deposit_value
+                element.update({"BankBalance":updated_Database_value})
+                return f'Your Bank Balance after deposit is {element["BankBalance"]}'
+        return None 
+    #print(DepositSetter(Database))    
+def Options():
+    print("""Enter Options: 
+    1) Check Bank Balance
+    2) Withdraw 
+    3) Deposit
+    4) Create Account
+    5) Search information of all the Bank Accounts """)
+    choose = int(input("Enter the Number of choice:"))
+    if choose == 1:
+        print(Customer.BankBalanceGetter(bank_data,Account_Number_value=int(input("Enter Account Number: "))))
+    elif choose == 2:
+        print(Customer.WithdrawSetter(bank_data,Account_Number_value=int(input("Enter Account Number: "))))
+    elif choose == 3:
+        print(Customer.DepositSetter(bank_data,Account_Number_value=int(input("Enter Account Number: "))))
+    elif choose == 4:
+        print(Bank.CreateAccount(bank_data,Account_Number_value=int(input("Enter Account Number: "))))
+    elif choose == 5:
+        print(Bank.Search_All_Account_Getter(bank_data))
+Options()
+print(bank_data)
