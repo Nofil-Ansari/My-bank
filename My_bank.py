bf3b4c35d19a9052f2db0cd229248501d9c0486a
Hashman <hashman@HashHost>
2025-02-27 23:53:11 +0500

    changed several things including removed syntax for keyword arguements only  and positional arguements only from parameters

--- My_bank.py
+++ My_bank.py
@@ -16,30 +16,30 @@ def menu():
     return input(textwrap.dedent(menu))
 
 # Function to make a deposit
-def deposit(balance, value, statement, /):
+def deposit(balance, value, statement):
     if value > 0:
         balance += value
         statement += f"Deposit:\t$ {value:.2f}\n"
         print("\n=== Deposit successful! ===")
     else:
-        print("\n@@@ Operation failed! The provided value is invalid. @@@")
+        print("\n Operation failed! The provided value is invalid. ")
 
     return balance, statement
 
 # Function to make a withdrawal
-def withdraw(*, balance, value, statement, limit, withdrawal_count, withdrawal_limit):
+def withdraw( balance, value, statement, limit, withdrawal_count, withdrawal_limit):
     exceeded_balance = value > balance   
     exceeded_limit = value > limit
     exceeded_withdrawals = withdrawal_count >= withdrawal_limit
 
     if exceeded_balance:
-        print("\n@@@ Operation failed! You do not have sufficient balance. @@@")
+        print("\n Operation failed! You do not have sufficient balance. ")
 
     elif exceeded_limit:
-        print("\n@@@ Operation failed! The withdrawal amount exceeds the limit. @@@")
+        print("\n Operation failed! The withdrawal amount exceeds the limit. ")
 
     elif exceeded_withdrawals:
-        print("\n@@@ Operation failed! Maximum number of withdrawals exceeded. @@@")
+        print("\n Operation failed! Maximum number of withdrawals exceeded. ")
 
     elif value > 0:
         balance -= value
@@ -48,12 +48,12 @@ def withdraw(*, balance, value, statement, limit, withdrawal_count, withdrawal_l
         print("\n=== Withdrawal successful! ===")
 
     else:
-        print("\n@@@ Operation failed! The provided value is invalid. @@@")
+        print("\n Operation failed! The provided value is invalid. ")
 
     return balance, statement, withdrawal_count
 
 # Function to display the account statement
-def display_statement(balance, /, *, statement):
+def display_statement(balance, statement):
     print("\n================ STATEMENT ================")
     print("No transactions have been made." if not statement else statement)
     print(f"\nBalance:\t$ {balance:.2f}")
@@ -61,37 +61,37 @@ def display_statement(balance, /, *, statement):
 
 # Function to create a new user
 def create_user(users):
-    cpf = input("Enter the CPF (numbers only): ")
-    user = filter_user(cpf, users)
+    IBAN = input("Enter the IBAN (numbers only): ")
+    user = filter_user(IBAN, users)
 
     if user:
-        print("\n@@@ User with this CPF already exists! @@@")
+        print("\n User with this IBAN already exists! ")
         return
 
     name = input("Enter the full name: ")
     birth_date = input("Enter the birth date (dd-mm-yyyy): ")
-    address = input("Enter the address (street, number - neighborhood - city/state abbreviation): ")
+    
 
     users.append({"name": name, "birth_date": birth_date,
-                  "cpf": cpf, "address": address})
+                  "IBAN": IBAN})
 
     print("=== User created successfully! ===")
 
-# Function to filter a user by CPF
-def filter_user(cpf, users):
-    filtered_users = [user for user in users if user["cpf"] == cpf]
+# Function to filter a user by IBAN
+def filter_user(IBAN, users):
+    filtered_users = [user for user in users if user["IBAN"] == IBAN]
     return filtered_users[0] if filtered_users else None
 
 # Function to create a new account
 def create_account(branch, account_number, users):
-    cpf = input("Enter the user's CPF (numbers only): ")
-    user = filter_user(cpf, users)
+    IBAN = input("Enter the user's IBAN (numbers only): ")
+    user = filter_user(IBAN, users)
 
     if user:
         print("\n=== Account created successfully! ===")
         return {"branch": branch, "account_number": account_number, "user": user}
 
-    print("\n@@@ User not found, account creation process terminated! @@@")
+    print("\n User not found, account creation process terminated! ")
 
 # Function to list all accounts
 def list_accounts(accounts):
