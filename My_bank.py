adb71853b66b9fc606a2247eb20c860c8c089081
Hashman <hashman@HashHost>
2025-02-28 05:52:19 +0500

    my functional programming  approach to Bank Management system

--- My_bank.py
+++ My_bank.py
@@ -1,67 +1,161 @@
-#A list is declared and each element of list is a Dictionary used as a database, named bank_data
-bank_data = [{"CustomerName": "Nofil", "AccountNumber":245645,"AccountType":"Savings Account","BankBalance":10000}, {"CustomerName": "Mujtaba", "AccountNumber":24555,"AccountType":"Current Account","BankBalance":1000}]
-#Account_Number_value = int(input("Enter Account number: ")) #Code written to debug just complete rubbish
+import textwrap
 
-class Bank: #Parent Class hai yeh 
-    def __init__(self,Database,Account_Number_value): #Initilization function hai yeh aur () main parameters hain 
-        self.database = Database #Instance Variable hai yeh, and the value assigned is of Parameters
-        self.Account_Number_value = Account_Number_value #Instance Variable hai yeh, and the value assigned is of Parameters
-    def Search_All_Account_Getter(Database): #Search Function made for retriving information of all bank accounts
-        for element in range(len(Database)):
-            print( Database[element])
-        
-    def CreateAccount(Database): #takes necessary data to make account and adds into database 
-        global bank_data
-        CustomerName = input("Enter Customer Name: ")
-        AccountNumber = int(input("Enter Account Number: "))
-        AccountType = input("Enter Account type Current Account or Saving Account?: ")
-        Deposit_Amount = int(input("Enter Bank Deposit amount: "))
-        Updated_dict = {"CustomerName":CustomerName,"AccountNumber":AccountNumber,"AccountType":AccountType,"Deposit_Amount":Deposit_Amount}
-        bank_data.append(Updated_dict)
-        return f'Your Account has been added with details {Updated_dict}'            
-class Customer(Bank): #This a child Class, it inherits some functionalities from, Bank class 
-    def __init__(self, Database): #This is the Initiliazation function of Class Customer
-        super().__init__(Database) #Yeh Inherited function hai Bank class ka 
-    def BankBalanceGetter(Database,Account_Number_value): #Returns Bank Balance of the searched Account number 
-        for element in Database: #Each Element is being compared with the searched Account number
-            if element['AccountNumber'] == Account_Number_value:  # If the search value matches, bank balance is printed               
-                return f'Your Bank  Balance is {element["BankBalance"]}'
-        return None 
-    def WithdrawSetter(Database,Account_Number_value): #it subtracts drawn value and returns the bank balance after 
-        withdraw_value = int(input("Enter withdraw amount: ")) 
-        for element in Database:
-            if element['AccountNumber'] == Account_Number_value:
-                updated_Database_value = element.get("BankBalance") - withdraw_value
-                element.update({"BankBalance":updated_Database_value})
-                return f'Your Bank Balance after withdraw is {element["BankBalance"]}'
-        return None 
-    #print(WithdrawSetter(Database)) #Code written for debugging purposes
-    def DepositSetter(Database,Account_Number_value): # it sums the value of deposited amount into the bank balance 
-        Deposit_value = int(input("Enter Deposit amount: "))
-        for element in Database:
-            if element['AccountNumber'] == Account_Number_value:
-                updated_Database_value = element.get("BankBalance") + Deposit_value
-                element.update({"BankBalance":updated_Database_value})
-                return f'Your Bank Balance after deposit is {element["BankBalance"]}'
-        return None 
-    #print(DepositSetter(Database))    #Code written for debugging
-def Options():
-    print("""Enter Options: 
-    1) Check Bank Balance
-    2) Withdraw 
-    3) Deposit
-    4) Create Account
-    5) Search information of all the Bank Accounts """)
-    choose = int(input("Enter the Number of choice:"))
-    if choose == 1:
-        print(Customer.BankBalanceGetter(bank_data,Account_Number_value=int(input("Enter Account Number: "))))
-    elif choose == 2:
-        print(Customer.WithdrawSetter(bank_data,Account_Number_value=int(input("Enter Account Number: "))))
-    elif choose == 3:
-        print(Customer.DepositSetter(bank_data,Account_Number_value=int(input("Enter Account Number: "))))
-    elif choose == 4:
-        print(Bank.CreateAccount(bank_data))
-    elif choose == 5:
-        Bank.Search_All_Account_Getter(bank_data)
-Options()
-#print(bank_data) #code written for debugging 
+
+# Function to display the menu and get user choice
+def menu():
+    menu = """\n
+    ================ MENU ================
+    [d]\tDeposit
+    [w]\tWithdraw
+    [s]\tStatement
+    [na]\tNew account
+    [la]\tList accounts
+    [nu]\tNew user
+    [q]\tQuit
+    => """
+    return input(textwrap.dedent(menu))
+
+# Function to make a deposit
+def deposit(balance, value, statement, /):
+    if value > 0:
+        balance += value
+        statement += f"Deposit:\t$ {value:.2f}\n"
+        print("\n=== Deposit successful! ===")
+    else:
+        print("\n@@@ Operation failed! The provided value is invalid. @@@")
+
+    return balance, statement
+
+# Function to make a withdrawal
+def withdraw(*, balance, value, statement, limit, withdrawal_count, withdrawal_limit):
+    exceeded_balance = value > balance   
+    exceeded_limit = value > limit
+    exceeded_withdrawals = withdrawal_count >= withdrawal_limit
+
+    if exceeded_balance:
+        print("\n@@@ Operation failed! You do not have sufficient balance. @@@")
+
+    elif exceeded_limit:
+        print("\n@@@ Operation failed! The withdrawal amount exceeds the limit. @@@")
+
+    elif exceeded_withdrawals:
+        print("\n@@@ Operation failed! Maximum number of withdrawals exceeded. @@@")
+
+    elif value > 0:
+        balance -= value
+        statement += f"Withdrawal:\t$ {value:.2f}\n"
+        withdrawal_count += 1
+        print("\n=== Withdrawal successful! ===")
+
+    else:
+        print("\n@@@ Operation failed! The provided value is invalid. @@@")
+
+    return balance, statement, withdrawal_count
+
+# Function to display the account statement
+def display_statement(balance, /, *, statement):
+    print("\n================ STATEMENT ================")
+    print("No transactions have been made." if not statement else statement)
+    print(f"\nBalance:\t$ {balance:.2f}")
+    print("==========================================")
+
+# Function to create a new user
+def create_user(users):
+    cpf = input("Enter the CPF (numbers only): ")
+    user = filter_user(cpf, users)
+
+    if user:
+        print("\n@@@ User with this CPF already exists! @@@")
+        return
+
+    name = input("Enter the full name: ")
+    birth_date = input("Enter the birth date (dd-mm-yyyy): ")
+    address = input("Enter the address (street, number - neighborhood - city/state abbreviation): ")
+
+    users.append({"name": name, "birth_date": birth_date,
+                  "cpf": cpf, "address": address})
+
+    print("=== User created successfully! ===")
+
+# Function to filter a user by CPF
+def filter_user(cpf, users):
+    filtered_users = [user for user in users if user["cpf"] == cpf]
+    return filtered_users[0] if filtered_users else None
+
+# Function to create a new account
+def create_account(branch, account_number, users):
+    cpf = input("Enter the user's CPF (numbers only): ")
+    user = filter_user(cpf, users)
+
+    if user:
+        print("\n=== Account created successfully! ===")
+        return {"branch": branch, "account_number": account_number, "user": user}
+
+    print("\n@@@ User not found, account creation process terminated! @@@")
+
+# Function to list all accounts
+def list_accounts(accounts):
+    for account in accounts:
+        line = f"""\
+            Branch:\t{account['branch']}
+            A/C:\t\t{account['account_number']}
+            Holder:\t{account['user']['name']}
+        """
+        print("=" * 100)
+        print(textwrap.dedent(line))
+
+# Main function
+def main():
+    WITHDRAWAL_LIMIT = 3
+    BRANCH = "0001"
+
+    balance = 0
+    limit = 500
+    statement = ""
+    withdrawal_count = 0
+    users = []
+    accounts = []
+    account_number = 1
+
+    while True:
+        option = menu()
+
+        if option == "d":
+            value = float(input("Enter the deposit amount: "))
+            balance, statement = deposit(balance, value, statement)
+
+        elif option == "w":
+            value = float(input("Enter the withdrawal amount: "))
+            balance, statement, withdrawal_count = withdraw(
+                balance=balance,
+                value=value,
+                statement=statement,
+                limit=limit,
+                withdrawal_count=withdrawal_count,
+                withdrawal_limit=WITHDRAWAL_LIMIT,
+            )
+
+        elif option == "s":
+            display_statement(balance, statement=statement)
+
+        elif option == "nu":
+            create_user(users)
+
+        elif option == "na":
+            account = create_account(BRANCH, account_number, users)
+
+            if account:
+                accounts.append(account)
+                account_number += 1
+
+        elif option == "la":
+            list_accounts(accounts)
+            
+        elif option == "q":
+            break
+
+        else:
+            print("Invalid operation, please select the desired operation again.")
+
+# Program execution
+main()
